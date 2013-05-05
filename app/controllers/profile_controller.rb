65c3e0c42f4c133ac9b34a9b0a373a524ec2f315
Mark Ryall <mark@ryall.name>
2013-05-05 23:02:32 +1000

    added editing of a few profile fields

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,9 @@
 class ProfileController < ApplicationController
-  def edit
+  def update
+    if current_person.update_attributes params[:person]
+      redirect_to profile_url, notice: 'Profile successfully updated.'
+    else
+      render :edit
+    end
   end
 end
