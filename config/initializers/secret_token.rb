bc84636b84860440f2df43ccd14134809d32faf5
Mark Ryall <mark@ryall.name>
2013-05-12 16:20:06 +1000

    added rails secret from env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Deets::Application.config.secret_token = 'ae7495a06ffd7c084828a4745cec132b46b57a8965b9d66751100b0829cdfbd21af524fc1de2fd2a8ad4dde5d017f1f62907fe687eab961a9dfe35b058fe0106'
+Deets::Application.config.secret_token = ENV['RAILS_SECRET'] || 'ae7495a06ffd7c084828a4745cec132b46b57a8965b9d66751100b0829cdfbd21af524fc1de2fd2a8ad4dde5d017f1f62907fe687eab961a9dfe35b058fe0106'
